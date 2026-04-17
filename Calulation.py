372a57f562c7ae74655a76e13ed22fa06381e9dc
Anurag-Codes-11 <anuragkori1978@gmail.com>
2026-04-17 18:16:05 +0530

    Changed the program

--- Calulation.py
+++ Calulation.py
@@ -1,2 +1,9 @@
-for i in range(1, 11):
-    print(i)
+x = int(input("Enter the mark of the student: "))
+if (85<=x and x<= 100):
+    print("Grade A")
+if (60<=x and x<= 85):
+    print("Grade B")
+if (40<=x and x<= 60):
+    print("Grade C")
+if (x>100 or x<0):
+    print("Invalid mark")
